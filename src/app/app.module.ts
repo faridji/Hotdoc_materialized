189f36c6226d06b69d4d4a47098282ce7b60deb5
faridullah_codegini <faridullah996@gmail.com>
2018-12-12 18:26:29 +0500

    Adding basics routes and navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { MaterialComponentsModule } from './material-components/material-components.module';
-
+import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
     AppComponent
@@ -10,6 +10,10 @@ import { MaterialComponentsModule } from './material-components/material-compone
   imports: [
     BrowserModule,
     MaterialComponentsModule,
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      { path: 'home', component: AppComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/material-components/material-components.module.ts
+++ src/app/material-components/material-components.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatCheckboxModule, MatIconModule, MatListModule, MatSidenavModule, MatToolbarModule, MatMenuModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 @NgModule({
   declarations: [],
   exports: [
     BrowserAnimationsModule,
     FlexLayoutModule,
-    MatButtonModule, MatCheckboxModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatMenuModule,
   ]
 })
 export class MaterialComponentsModule { }
